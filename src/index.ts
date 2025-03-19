a9264654be30c86826bcd4ac0c0bfc1930396dc2
Ankit Chadoker <ankitchadoker.dev@gmail.com>
2025-03-19 10:22:26 +0530

    test for: handling line breaker and comma both as string separator.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export function add(numbers: string): number {
   if (!numbers) return 0;
-  const allNumbers = numbers.split(",").map((number) => parseInt(number, 10));
+  const allNumbers = numbers
+    .split(/,|\n/)
+    .map((number) => parseInt(number, 10));
   const negatives = allNumbers.filter((n) => n < 0);
   if (negatives.length > 0) {
     throw new Error(`negative numbers are not allowed: ${negatives.join(",")}`);
