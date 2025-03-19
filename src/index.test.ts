e12cea3948d2ec21d0a7eb5fc054749db911183b
Ankit Chadoker <ankitchadoker.dev@gmail.com>
2025-03-19 10:03:31 +0530

    test for: individual number would return itself

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,3 +3,9 @@ import { add } from "./index";
 test("empty string would return 0", () => {
   expect(add("")).toBe(0);
 });
+
+test("individual number would return itself", () => {
+  expect(add("1")).toBe(1);
+  expect(add("2")).toBe(2);
+  expect(add("10")).toBe(10);
+});
