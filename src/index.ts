d0e0c2e2c91a969cd326042e1307553c481dcbcb
Ankit Chadoker <ankitchadoker.dev@gmail.com>
2025-03-27 18:59:40 +0530

    removed the commented code.

--- src/index.ts
+++ src/index.ts
@@ -13,19 +13,14 @@ export function add(numbers: string): number {
 
   const allNumbers = numbers.split(separator).map((number) => number);
 
-  // console.log(allNumbers);
   const invalidChar = allNumbers.filter((n) => isNaN(+n));
-  // console.log(invalidChar);
-  // return 1;
 
   if (invalidChar.length > 0) {
     throw new Error(
       `invalid numbers are encountered: ${invalidChar.join(",")}`
     );
   }
-  // const validNumbers = allNumbers.filter((n) => !isNaN(+n));
-  // console.log(validNumbers);
-  // return 1;
+
   const negatives = allNumbers.filter((n) => +n < 0);
   if (negatives.length > 0) {
     throw new Error(`negative numbers are not allowed: ${negatives.join(",")}`);
@@ -33,5 +28,3 @@ export function add(numbers: string): number {
 
   return allNumbers.reduce((sum, number) => sum + +number, 0);
 }
-
-// console.log(add("1,2,-3,4"));
